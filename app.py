3ebbda7e3099af22e70f75d2e1e11ea0072fb3a1
DESKTOP-0URQDAI\Owner <limjh>
2024-12-20 17:08:14 +0900

    [API 환경 구축] 패키지 설치 및 캡슐화를 위한 env 환경 세

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-# Description: Flask 애플리케이션 메인 파일.
-
 from flask import Flask, send_from_directory
+from dotenv import load_dotenv
+import os
+
+# 환경 변수 로드
+load_dotenv()
 
 app = Flask(__name__, static_folder="frontend", static_url_path="")
 
 @app.route('/')
+
 def serve_frontend():
     # 프론트엔드의 index.html 제공
-    return send_from_directory("frontend", "index.html")  
-  
+    return send_from_directory("frontend", "index.html")
+
 @app.route('/<path:path>')
 def serve_static_files(path):
     # 프론트엔드의 정적 파일 제공
-    return send_from_directory("frontend", path) 
+    return send_from_directory("frontend", path)
 
 if __name__ == '__main__':
     # Flask 서버 실행
